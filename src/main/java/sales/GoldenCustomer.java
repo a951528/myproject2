fd297647307ce0c040d3f663a4ba2d20d28ea082
a951528 <a951528@gmail.com>
2022-04-22 08:56:13 +0800

    Customer

---
+++ src/main/java/sales/GoldenCustomer.java
@@ -0,0 +1,18 @@
+package sales;
+
+public class GoldenCustomer extends SilverCustomer{
+
+    public GoldenCustomer(int id,int consumption){
+        super(id,consumption);
+    }
+
+    public float Getpoint(){
+        return (consumption/1000)*2*off*1000;
+    }
+
+    public void print(){
+        System.out.print(id + "\t" + consumption + "\t" + GetEndmoney()+ "\t" + Getpoint());
+
+    }
+
+}
---
+++ src/main/java/sales/main.java
@@ -0,0 +1,31 @@
+package sales;
+
+public class main {
+
+    public static void main(String[] args) {
+/*
+        Customer Amy = new Customer();
+        Amy.consumption = 1000;
+        System.out.print("Pay:");
+        //System.out.print(Amy.cost(1000));
+*/
+        Customer Amy = new Customer(1,1200);
+        Amy.print();
+
+        System.out.println();
+
+        //2200---->2000??
+        Customer Tina = new SilverCustomer(2,2200);
+        Tina.print();
+
+        System.out.println();
+
+        Customer Helen = new GoldenCustomer(3,3600);
+        Helen.print();
+
+
+
+
+    }
+
+}
