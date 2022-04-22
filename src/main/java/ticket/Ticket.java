9451f626711e38e5c4cbef2dc60c68494d1d743c
a951528 <a951528@gmail.com>
2022-04-22 09:08:03 +0800

    Ticket

---
+++ src/main/java/ticket/Ticket.java
@@ -0,0 +1,34 @@
+package ticket;
+
+public class Ticket {
+
+    //int price_Taipei_to_Taichung;
+    public static final int TAIPEI_CITY = 100;
+    public static final int TAICHUNG_CITY = 200;
+    public static final int KAOHSIUNG_CITY = 300;
+
+    int price;
+    //int start;
+    //int end;
+    Station start;
+    Station end;
+
+    public Ticket(Station start, Station end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public void print() {
+//        taipeiCity = 100;
+    }
+
+
+
+
+
+    //public Passenger(int start,int end){}
+
+
+
+
+}
